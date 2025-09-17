0d2be29172d948d327c80f746040482f399a40f5
santhosh <santhosh.003z@gmail.com>
2025-09-17 11:57:59 +0530

    fix: updated variable name

--- src/components/ui/Stack.tsx
+++ src/components/ui/Stack.tsx
@@ -1,14 +1,14 @@
 import type { stack } from "../../types/types";
 import TextTag from "./TextTag";
 
-function Stack({ stack }: { stack: stack }) {
+function Stack({ techStack }: { techStack: stack }) {
   return (
     <>
       <div className="flex flex-col gap-4 mt-[5px]">
-        <h3>{stack.title}</h3>
+        <h3>{techStack.title}</h3>
         <div className="flex flex-wrap gap-2 text-(--gray-dark)">
-          {stack.stack.map((s) => (
-            <TextTag text={s} />
+          {techStack.stack.map((s) => (
+            <TextTag text={String(s)} />
           ))}
         </div>
       </div>
