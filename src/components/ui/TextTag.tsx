5649e25ac1a628ec32cd8ebaccb5276d1071628f
santhosh <santhosh.003z@gmail.com>
2025-11-25 23:02:30 +0530

    reduced border radius

--- src/components/ui/TextTag.tsx
+++ src/components/ui/TextTag.tsx
@@ -1,6 +1,6 @@
 function TextTag({ text }: { text: string }) {
   return (
-    <div className="text-xs font-(family-name:--font-mono) text-(--gray-light) bg-(--gray-dark) rounded-lg py-1 px-2 cursor-default">
+    <div className="text-xs font-(family-name:--font-mono) text-(--gray-light) bg-(--gray-dark) rounded-sm py-1 px-2 cursor-default">
       {text}
     </div>
   );
